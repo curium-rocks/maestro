9028b82170d1725ec7d6e2e5d420767431e1c927
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-08-14 16:01:21 -0500

    interface stub imps

--- src/maestro.ts
+++ src/maestro.ts
@@ -5,54 +5,89 @@ import { IDisposable } from '@curium.rocks/data-emitter-base/build/src/dataEmitt
 /**
  * IMaestro implementation
  */
-export class Maestro implements IMaestro, IService {
-
+export class Maestro implements IMaestro, IService, IDisposable {
     /**
      * 
      */
-    emitters: Iterable<IDataEmitter>;
-    chroniclers: Iterable<IChronicler>;
-
-    connect(emitters: Iterable<IDataEmitter>, chroniclers: Iterable<IChronicler>): IDisposable {
-
-    }
-
-    connect(emitter: IDataEmitter, chronicler: IChronicler): IDisposable {
-
-    }
-    
-    connect(emitter: any, chronicler: any): IDisposable {
-        throw new Error('Method not implemented.');
-    }
-    
-    addEmitter(emitter: IDataEmitter): void {
-        throw new Error('Method not implemented.');
-    }
-    
-    removeEmitter(emitter: IDataEmitter): void {
-        throw new Error('Method not implemented.');
-    }    
-    
-    addChronicler(chronicler: IChronicler): void {
-        throw new Error('Method not implemented.');
-    }
-    
-    removeChronicler(chronicler: IChronicler): void {
-        throw new Error('Method not implemented.');
-    }
-
-    load(): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-
-    save(): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-
     start(): Promise<void> {
         throw new Error('Method not implemented.');
     }
+    /**
+     * 
+     */
     stop(): Promise<void> {
         throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     */
+    dispose(): void {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     * @param {IDataEmitter|Iterable<IDataEmitter>} emitters 
+     * @param {IChronicler|Iterable<IChronicler>} chroniclers 
+     */
+    connect(emitters: IDataEmitter | Iterable<IDataEmitter>, chroniclers: IChronicler | Iterable<IChronicler>): IDisposable {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     */
+    load(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     */
+    save(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     * @param {IDataEmitter}  emitter 
+     */
+    addEmitter(emitter: IDataEmitter): void {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     * @param {IDataEmitter} emitter 
+     */
+    removeEmitter(emitter: IDataEmitter): void {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     * @param {IChronicler} chronicler 
+     */
+    addChronicler(chronicler: IChronicler): void {
+        throw new Error('Method not implemented.');
+    }
+    /**
+     * 
+     * @param {IChronicler} chronicler 
+     */
+    removeChronicler(chronicler: IChronicler): void {
+        throw new Error('Method not implemented.');
+    }
+    
+
+    private readonly _emitters: Map<string, IDataEmitter> = new Map<string, IDataEmitter>();
+    private readonly _chroniclers: Map<string, IChronicler> = new Map<string, IChronicler>();
+
+    /**
+     * @return {Iterable<IDataEmitter>}
+     */
+    get emitters() : Iterable<IDataEmitter> {
+        return this._emitters.values();
+    }
+
+    /**
+     * @return {Iterable<IChronicler>}
+     */
+    get chroniclers() : Iterable<IChronicler> {
+        return this._chroniclers.values();
     }
 }
